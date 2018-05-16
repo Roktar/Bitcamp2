1a669646c8796a39423df5a9f63e9b16877ce38c
donhee <231313do@gmail.com>
2018-05-16 15:06:42 +0900

    donhee

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
@@ -11,7 +11,7 @@ public class Test {
         
         ServerRequest request = new ServerRequest(str);
         
-        System.out.println("hello~~~ 한돈희");
+        System.out.println("hello~~~ 한돈희22");
         System.out.println(request.getServerPath());
         System.out.println(request.getParameter("title"));
         System.out.println(request.getParameter("content"));
