66d245e4658628a3aa2e9c87dfcb170d5ccc4623
haeunjang <ha.eunhany@gmail.com>
2018-05-16 15:49:58 +0900

    d

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/member/MemberListController.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/member/MemberListController.java
@@ -37,7 +37,7 @@ public class MemberListController extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-       
+        
         out.println("<!DOCTYPE html>");
         out.println("<html>");
         out.println("<head>");
