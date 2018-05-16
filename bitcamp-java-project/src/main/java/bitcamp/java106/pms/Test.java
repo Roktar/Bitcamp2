a663a481d07bfd1a328b23bdb8de889899fd0d69
dhlee3814 <dhlee3814@gmail.com>
2018-05-16 14:40:50 +0900

    jn

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/Test.java
@@ -8,15 +8,7 @@ public class Test {
         // 프로그램을 짜다 보면 특정 API를 사용할 때가 있다.
         // 그 API를 적용하기 전에 간단한 예제를 만들어 동작을 확인하라!
         String str = "/board/add?title=aaaa&content=bbb";
-        
-        ServerRequest request = new ServerRequest(str);
-        
-        System.out.println(request.getServerPath());
-        System.out.println(request.getParameter("title"));
-        System.out.println(request.getParameter("content"));
-        System.out.println(request.getParameter("age"));
-        
-
+        System.out.println("d");
     }
 
 }
